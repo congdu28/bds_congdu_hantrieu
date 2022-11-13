c9f087877f2c71029c4633e4b110ba96184eb7fe
congdu28 <102013600+congdu28@users.noreply.github.com>
2022-11-13 23:41:46 +0700

    Initial commit

---
+++ database/migrations/2022_11_13_162145_create_banner_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('banner', function (Blueprint $table) {
+            $table->id();
+            $table->string('banner_name');
+            $table->string('content');
+            $table->string('address');
+            $table->string('show_pages');
+            $table->string('links');
+            $table->string('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('banner');
+    }
+};
