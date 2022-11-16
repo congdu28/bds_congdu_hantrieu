252b8db594e3e1f8253886bbda6b99fdb228df2a
congdu28 <102013600+congdu28@users.noreply.github.com>
2022-11-16 13:04:42 +0700

    create advise table

---
+++ database/migrations/2022_11_16_060131_create_advise_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('advise', function (Blueprint $table) {
+            $table->id();
+            $table->string('id_projects');
+            $table->string('id_users');
+            $table->string('phone_users');
+            $table->string('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('advise');
+    }
+};
